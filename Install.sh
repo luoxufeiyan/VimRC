7de9899a9685d29887573137dfd5a85f4f89398a
LXFY <luoxufeiyan@gmail.com>
2019-07-03 12:19:59 +1000

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+# install vim and vundle
 sudo apt-get install vim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/vundle
-cp ~/.vim/.vimrc ~/.vimrc
+
+# vim config
+cp .vimrc ~/.vimrc
+
+# vim theme
+cp -R colors/ ~/.vim/
+# vim plugins
+cp -R bundle/ ~/.vim/
+cp -R autoload/ ~/.vim/
